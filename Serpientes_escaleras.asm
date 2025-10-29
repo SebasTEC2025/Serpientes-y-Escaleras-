7bf44726abe5ea3eca2072ffab9f1d9d53c0ee2b
SebasTEC2025 <s.sanchez.11@estudiantec.cr>
2025-10-29 13:05:41 -0600

    arreglos generales

--- Serpientes_escaleras.asm
+++ Serpientes_escaleras.asm
@@ -5,22 +5,25 @@ extern srand
 extern time
 
 section .data
-    mensaje_inicio        db "La Senda: Serpientes y Escalones - Tablero 10x10", 10, 0
-    mensaje_pedir_jug     db "Ingrese numero de jugadores (2-5 jugadores): ", 0
-    mensaje_turno         db "Jugador %d, presione ENTER para tirar el dado...", 10, 0
-    mensaje_dado          db "Valor del dado: %d", 10, 0
-    mensaje_posicion      db "Jugador %d movido a casilla %d", 10, 0
-    mensaje_escalon       db "Escalon: sube de %d a %d", 10, 0
-    mensaje_serpiente     db "Serpiente: baja de %d a %d", 10, 0
-    mensaje_movimientos   db "Movimientos Jugador %d: %d", 10, 0
-    mensaje_campeon       db "¡Campeon! Jugador %d en %d movimientos.", 10, 0
-    mensaje_finales       db "Estados finales de los jugadores:", 10, 0
-    mensaje_rival         db "Rival %d quedo en casilla %d", 10, 0
-    mensaje_reiniciar     db "¿Desea reiniciar la partida? (1=Si, 0=No): ", 0
-    formato_entero        db "%d", 0
-    formato_enter         db "%c", 0
+    msg_inicio         db "la senda: serpientes y escaleras - tablero 10x10", 10, 0
+    msg_pedir_jug      db "ingrese numero de jugadores (1-3): ", 0
+    msg_turno          db "jugador %d, presione enter para tirar el dado...", 10, 0
+    msg_dado           db "valor del dado: %d", 10, 0
+    msg_posicion       db "jugador %d movido a casilla %d", 10, 0
+    msg_escalera       db "escalera: sube de %d a %d", 10, 0
+    msg_serpiente      db "serpiente: baja de %d a %d", 10, 0
+    msg_movimientos    db "movimientos jugador %d: %d", 10, 0
+    msg_ganador        db "campeon jugador %d en %d movimientos", 10, 0
+    msg_finales        db "estado final de los jugadores:", 10, 0
+    msg_rival          db "rival %d quedo en casilla %d", 10, 0
+    msg_reiniciar      db "desea reiniciar la partida? (1=si, 0=no): ", 0
 
-    simbolos_jugadores    db "ABCDE"
+    fmt_entero         db "%d", 0
+    fmt_enter          db "%c", 0
+    fmt_char           db "%c ", 0
+    salto_linea_texto  db 10, 0
+
+    simbolos_jug       db "abc"
 
     tablero_base db \
         0,0,0,0,0,0,0,0,0,0,\
@@ -34,33 +37,27 @@ section .data
         0,0,0,0,0,0,0,0,0,0,\
         0,0,0,0,0,0,0,0,0,0
 
-    escaleras_origen_dd dd 3, 23, 43
-    escaleras_destino_dd dd 13, 33, 63
-    escaleras_inicio_db db 3, 23, 43
-    escaleras_fin_db db 13, 33, 63
+    escaleras_ini_db   db 3, 23, 43
+    escaleras_fin_db   db 13, 33, 63
+    escaleras_ini_dd   dd 3, 23, 43
+    escaleras_fin_dd   dd 13, 33, 63
 
-    serpientes_origen_dd dd 59, 79, 99
-    serpientes_destino_dd dd 51, 71, 91
-    serpientes_inicio_db db 59, 79, 99
-    serpientes_fin_db db 51, 71, 91
-
-    nueva_linea db 10,0
-    fmt_char db "%c ",0
-
-SEBAS
+    serpientes_ini_db  db 59, 79, 99
+    serpientes_fin_db  db 51, 71, 91
+    serpientes_ini_dd  dd 59, 79, 99
+    serpientes_fin_dd  dd 51, 71, 91
 
 section .bss
-    num_jugadores     resd 1
-    posiciones        resd 5
-    movimientos       resd 5
-    buffer_enter      resb 4
-    jugador_actual    resd 1
-    tick_semilla      resd 1
-    flag_reiniciar    resd 1
-    tablero           resb 100
-    indice_ganador    resd 1
+    jug_total          resd 1
+    jug_posiciones     resd 3
+    jug_movimientos    resd 3
+    buffer_enter       resb 4
+    turno_actual       resd 1
+    semilla_tick       resd 1
+    bandera_reinicio   resd 1
+    tablero            resb 100
+    ganador_indice     resd 1
 
-; Rutina: dibuja el tablero en pantalla
 section .text
     global main
 
@@ -71,37 +68,65 @@ dibujar_tablero:
     push ecx
     push edx
 
-    xor esi, esi
+    mov esi, 9
 fila_loop:
-    cmp esi, 10
-    jge dibujar_terminado
+    ; terminar cuando esi < 0 (imprime filas 9..0)
+    cmp esi, 0
+    jl dibujar_terminado
 
-    xor edi, edi
-columna_loop:
-    cmp edi, 10
-    jge salto_linea
+    mov ecx, esi
+    and ecx, 1
+    cmp ecx, 0
+    je izquierda_a_derecha
 
+derecha_a_izquierda:
+    mov edi, 9
+columna_loop_derecha:
+    ; terminar cuando edi < 0 (imprime columnas 9..0)
+    cmp edi, 0
+    jl salto_linea
     mov eax, esi
     imul eax, 10
     add eax, edi
     mov al, [tablero + eax]
     cmp al, 0
-    jne imprimir_caracter
+    jne imprimir_caracter_derecha
     mov al, '.'
-imprimir_caracter:
+imprimir_caracter_derecha:
+    movzx eax, al
+    push eax
+    push fmt_char
+    call printf
+    add esp, 8
+    dec edi
+    jmp columna_loop_derecha
+
+izquierda_a_derecha:
+    mov edi, 0
+columna_loop_izquierda:
+    cmp edi, 10
+    jge salto_linea
+    mov eax, esi
+    imul eax, 10
+    add eax, edi
+    mov al, [tablero + eax]
+    cmp al, 0
+    jne imprimir_caracter_izquierda
+    mov al, '.'
+imprimir_caracter_izquierda:
     movzx eax, al
     push eax
     push fmt_char
     call printf
     add esp, 8
     inc edi
-    jmp columna_loop
+    jmp columna_loop_izquierda
 
 salto_linea:
-    push nueva_linea
+    push salto_linea_texto
     call printf
     add esp, 4
-    inc esi
+    dec esi
     jmp fila_loop
 
 dibujar_terminado:
@@ -112,6 +137,7 @@ dibujar_terminado:
     pop esi
     ret
 
+
 ; Rutina: quita marcas de jugadores del tablero
 limpiar_marcas_jugadores:
     push esi
@@ -120,15 +146,11 @@ limpiar_marcas_jugadores:
     xor esi, esi
 limpieza_loop:
     mov al, [tablero + esi]
-    cmp al, 'A'
+    cmp al, 'a'
     je poner_cero
-    cmp al, 'B'
+    cmp al, 'b'
     je poner_cero
-    cmp al, 'C'
-    je poner_cero
-    cmp al, 'D'
-    je poner_cero
-    cmp al, 'E'
+    cmp al, 'c'
     je poner_cero
     jmp siguiente_celda
 poner_cero:
@@ -157,92 +179,91 @@ copia_tablero:
     mov ecx, 3
     xor esi, esi
 marcar_escaleras:
-    movzx eax, byte [escaleras_inicio_db + esi]
-    mov [tablero + eax], '^'
+    movzx eax, byte [escaleras_ini_db + esi]
+    mov byte [tablero + eax], '^'
     movzx eax, byte [escaleras_fin_db + esi]
-    mov [tablero + eax], 'U'
+    mov byte [tablero + eax], 'U'
     inc esi
     loop marcar_escaleras
 
     mov ecx, 3
     xor esi, esi
 marcar_serpientes:
-    movzx eax, byte [serpientes_inicio_db + esi]
-    mov [tablero + eax], 'S'
+    movzx eax, byte [serpientes_ini_db + esi]
+    mov byte [tablero + eax], 'S'
     movzx eax, byte [serpientes_fin_db + esi]
-    mov [tablero + eax], 'D'
+    mov byte [tablero + eax], 'D'
     inc esi
     loop marcar_serpientes
 
-    push mensaje_inicio
+    push msg_inicio
     call printf
     add esp, 4
 
-    push mensaje_pedir_jug
+    push msg_pedir_jug
     call printf
     add esp, 4
 
-    push num_jugadores
-    push formato_entero
+    push jug_total
+    push fmt_entero
     call scanf
     add esp, 8
 
-    mov eax, [num_jugadores]
-    cmp eax, 2
+    mov eax, [jug_total]
+    cmp eax, 1
     jl fin_partida
-    cmp eax, 5
+    cmp eax, 3
     jg fin_partida
 
     xor ecx, ecx
 inicializar_jugadores:
-    mov dword [posiciones + ecx*4], 0
-    mov dword [movimientos + ecx*4], 0
+    mov dword [jug_posiciones + ecx*4], 0
+    mov dword [jug_movimientos + ecx*4], 0
     inc ecx
-    mov eax, [num_jugadores]
+    mov eax, [jug_total]
     cmp ecx, eax
     jl inicializar_jugadores
 
-    mov dword [jugador_actual], 0
-    mov dword [tick_semilla], 0
+    mov dword [turno_actual], 0
+    mov dword [semilla_tick], 0
 
-; Ciclo principal del juego
 ciclo_partida:
     call limpiar_marcas_jugadores
-    mov ecx, [num_jugadores]
+    mov ecx, [jug_total]
     xor esi, esi
 colocar_jugadores:
-    mov eax, [posiciones + esi*4]
-    movzx edx, byte [simbolos_jugadores + esi]
-    mov [tablero + eax], dl
+    mov eax, [jug_posiciones + esi*4]
+    movzx edx, byte [simbolos_jug + esi]
+    mov byte [tablero + eax], dl
     inc esi
     cmp esi, ecx
     jl colocar_jugadores
 
     call dibujar_tablero
 
-    mov ecx, [jugador_actual]
+    mov ecx, [turno_actual]
     mov eax, ecx
     inc eax
     push eax
-    push mensaje_turno
+    push msg_turno
     call printf
     add esp, 8
 
     lea eax, [buffer_enter]
     push eax
-    push formato_enter
+    push fmt_enter
     call scanf
     add esp, 8
 
-    mov eax, [tick_semilla]
+    mov eax, [semilla_tick]
     inc eax
-    mov [tick_semilla], eax
+    mov [semilla_tick], eax
 
     push 0
     call time
     add esp, 4
     mov ebx, eax
-    mov eax, [tick_semilla]
+    mov eax, [semilla_tick]
     add ebx, eax
     push ebx
     call srand
@@ -257,36 +278,36 @@ colocar_jugadores:
     mov ebx, eax
 
     push ebx
-    push mensaje_dado
+    push msg_dado
     call printf
     add esp, 8
 
-    mov ecx, [jugador_actual]
-    mov eax, [posiciones + ecx*4]
+    mov ecx, [turno_actual]
+    mov eax, [jug_posiciones + ecx*4]
     add eax, ebx
     cmp eax, 99
     jle sin_ganar_directo
-    mov dword [posiciones + ecx*4], 99
-    mov eax, [jugador_actual]
-    mov [indice_ganador], eax
+    mov dword [jug_posiciones + ecx*4], 99
+    mov eax, [turno_actual]
+    mov [ganador_indice], eax
     jmp anunciar_ganador
 
 sin_ganar_directo:
-    mov dword [posiciones + ecx*4], eax
+    mov dword [jug_posiciones + ecx*4], eax
 
     xor esi, esi
 ver_escalones:
     cmp esi, 3
     jge ver_serpientes
-    mov eax, [posiciones + ecx*4]
-    cmp eax, dword [escaler as_origen_dd + esi*4] ; ajustar si su ensamblador requiere nombre exacto
+    mov eax, [jug_posiciones + ecx*4]
+    cmp eax, dword [escaleras_ini_dd + esi*4]
     jne siguiente_escalon
-    mov eax, dword [escaler as_destino_dd + esi*4]
-    mov [posiciones + ecx*4], eax
-    mov edx, [escaler as_origen_dd + esi*4]
+    mov eax, dword [escaleras_fin_dd + esi*4]
+    mov [jug_posiciones + ecx*4], eax
+    mov edx, [escaleras_ini_dd + esi*4]
     push eax
     push edx
-    push mensaje_escalon
+    push msg_escalera
     call printf
     add esp, 12
     jmp ver_serpientes
@@ -299,15 +320,15 @@ ver_serpientes:
 loop_serpientes:
     cmp esi, 3
     jge despues_eventos
-    mov eax, [posiciones + ecx*4]
-    cmp eax, dword [serpientes_origen_dd + esi*4]
+    mov eax, [jug_posiciones + ecx*4]
+    cmp eax, dword [serpientes_ini_dd + esi*4]
     jne siguiente_serpiente
-    mov eax, dword [serpientes_destino_dd + esi*4]
-    mov [posiciones + ecx*4], eax
-    mov edx, [serpientes_origen_dd + esi*4]
+    mov eax, dword [serpientes_fin_dd + esi*4]
+    mov [jug_posiciones + ecx*4], eax
+    mov edx, [serpientes_ini_dd + esi*4]
     push eax
     push edx
-    push mensaje_serpiente
+    push msg_serpiente
     call printf
     add esp, 12
     jmp despues_eventos
@@ -316,55 +337,55 @@ siguiente_serpiente:
     jmp loop_serpientes
 
 despues_eventos:
-    mov eax, [movimientos + ecx*4]
+    mov eax, [jug_movimientos + ecx*4]
     inc eax
-    mov [movimientos + ecx*4], eax
+    mov [jug_movimientos + ecx*4], eax
 
-    mov eax, [posiciones + ecx*4]
-    mov edx, [jugador_actual]
+    mov eax, [jug_posiciones + ecx*4]
+    mov edx, [turno_actual]
     inc edx
     push eax
     push edx
-    push mensaje_posicion
+    push msg_posicion
     call printf
     add esp, 12
 
-    mov eax, [movimientos + ecx*4]
-    mov edx, [jugador_actual]
+    mov eax, [jug_movimientos + ecx*4]
+    mov edx, [turno_actual]
     inc edx
     push eax
     push edx
-    push mensaje_movimientos
+    push msg_movimientos
     call printf
     add esp, 12
 
-    mov eax, [posiciones + ecx*4]
+    mov eax, [jug_posiciones + ecx*4]
     cmp eax, 99
     jne avanzar_turno
 
-    mov eax, [jugador_actual]
-    mov [indice_ganador], eax
+    mov eax, [turno_actual]
+    mov [ganador_indice], eax
     jmp anunciar_ganador
 
 avanzar_turno:
-    mov eax, [jugador_actual]
+    mov eax, [turno_actual]
     inc eax
-    mov ecx, [num_jugadores]
+    mov ecx, [jug_total]
     cmp eax, ecx
     jl guardar_turno
     xor eax, eax
 guardar_turno:
-    mov [jugador_actual], eax
+    mov [turno_actual], eax
     jmp ciclo_partida
 
 ; Mostrar resultados y preguntar reinicio
 anunciar_ganador:
     call limpiar_marcas_jugadores
-    mov ecx, [num_jugadores]
+    mov ecx, [jug_total]
     xor esi, esi
 colocar_final:
-    mov eax, [posiciones + esi*4]
-    movzx edx, byte [simbolos_jugadores + esi]
+    mov eax, [jug_posiciones + esi*4]
+    movzx edx, byte [simbolos_jug + esi]
     mov [tablero + eax], dl
     inc esi
     cmp esi, ecx
@@ -372,34 +393,40 @@ colocar_final:
 
     call dibujar_tablero
 
-    mov eax, [indice_ganador]
+    mov eax, [ganador_indice]
     inc eax
-    mov ebx, [indice_ganador]
-    mov ebx, [movimientos + ebx*4]
+    mov ebx, [ganador_indice]
+    mov ebx, [jug_movimientos + ebx*4]
     push ebx
     push eax
-    push mensaje_campeon
+    push msg_ganador
     call printf
     add esp, 12
 
-    push mensaje_finales
+    push msg_finales
     call printf
     add esp, 4
 
     xor esi, esi
-    mov ebx, [indice_ganador]
+    mov ebx, [ganador_indice]
+    mov eax, [jug_total]
+    cmp eax, 1
+    je preguntar_reiniciar
+
+    xor esi, esi
+    mov ebx, [ganador_indice]
 lista_finales:
     mov eax, esi
-    cmp eax, [num_jugadores]
+    cmp eax, [jug_total]
     jge preguntar_reiniciar
     cmp esi, ebx
     je omitir_ganador
-    mov edx, [posiciones + esi*4]
+    mov edx, [jug_posiciones + esi*4]
     mov eax, esi
     inc eax
     push edx
     push eax
-    push mensaje_rival
+    push msg_rival
     call printf
     add esp, 12
 omitir_ganador:
@@ -407,18 +434,24 @@ omitir_ganador:
     jmp lista_finales
 
 preguntar_reiniciar:
-    push mensaje_reiniciar
+    push msg_reiniciar
     call printf
     add esp, 4
-    push flag_reiniciar
-    push formato_entero
+
+    push bandera_reinicio
+    push fmt_entero
     call scanf
     add esp, 8
-    mov eax, [flag_reiniciar]
+
+    mov eax, [bandera_reinicio]
     cmp eax, 1
-    je inicio_partida
+    jne fin_partida
+
+    ; reiniciar: volver a inicio_partida (si deseas reinicializar más cosas, añadir aquí)
+    jmp inicio_partida
 
 fin_partida:
-    mov esp, ebp
-    pop ebp
-    ret
+    ; Restaurar pila/registro y terminar
+    mov eax, 0
+    leave
+    ret
